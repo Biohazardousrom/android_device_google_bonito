f3c734e537354c1b2ba2083feeb2b28793033e0c
John Galt <johngaltfirstrun@gmail.com>
2020-10-16 10:50:23 -0400

    correct microg typo
    Change-Id: Iea0ad37473b234813ef9632d049f403927f28b95

--- device.mk
+++ device.mk
@@ -985,4 +985,4 @@ PRODUCT_PACKAGES += \
 # $(call inherit-product, vendor/gapps/common/common-vendor.mk)
 
 # Microg
-$(call inherit-product, vendor/microg/microg-vendor.mk_)
+$(call inherit-product, vendor/microg/microg-vendor.mk)
